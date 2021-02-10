8b328755adf876d134a345bcbc33f9be56d0f9e0
Jude K <judekuti@gmail.com>
2021-02-10 13:14:28 -0600

    Complete swipeable-routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, useLocation } from 'react-router-dom';
+import SwipeableRoutes from 'react-swipeable-routes';
+
+const RedView = () => (
+  <div style={{ height: 300, backgroundColor: "red" }}>Red</div>
+);
+const BlueView = () => {
+  const {state} = useLocation();
+  return <>
+  <div style={{ height: 300, backgroundColor: "blue" }}>Blue: {}</div>
+  </>
+};
+const GreenView = () => (
+  <div style={{ height: 300, backgroundColor: "green" }}>Green</div>
+);
+const YellowView = () => (
+  <div style={{ height: 300, backgroundColor: "yellow" }}>Yellow</div>
+);
+const OtherColorView = ({ match }) => (
+  <div style={{ height: 300, backgroundColor: match.params.color }}>
+    {match.params.color}
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Router>
+          <div>
+            <div>
+              <h2>React Swipeable Routes example</h2>
+            </div>
+
+            <div>
+              <Link to="/red">Red</Link> |<Link to={{
+                pathname: "/blue",
+                state: {data: "Jude"}
+              }}>Blue</Link> |
+            <Link to="/green">Green</Link> |<Link to="/yellow">Yellow</Link> |
+            <Link to="/other/palevioletred">Pale Violet Red</Link> |
+            <Link to="/other/saddlebrown">Saddle Brown</Link>
+            </div>
+
+            <SwipeableRoutes replace>
+              <Route path="/red" component={RedView} />
+              <Route path="/blue" component={BlueView} />
+              <Route path="/green" component={GreenView} />
+              <Route path="/yellow" component={YellowView} />
+              <Route
+                path="/other/:color"
+                component={OtherColorView}
+                defaultParams={{ color: "grey" }}
+              />
+            </SwipeableRoutes>
+          </div>
+        </Router>
       </header>
     </div>
   );
